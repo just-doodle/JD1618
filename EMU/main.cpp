fa77e3fc83502c0f5a8216ba627450a2e90cce0f
Arun Krishna <69564895+Arun007coder@users.noreply.github.com>
2021-11-10 21:29:39 +0530

    Update main.cpp
    Added licence boilerplate to main.cpp

--- EMU/main.cpp
+++ EMU/main.cpp
@@ -1,4 +1,13 @@
 
+/*
+    JD1618  Copyright (C) 2021  Arun krishna
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.
+
+*/
+
+
 #include "CPU1618.h"
 #include <ncurses.h>
 #include <stdlib.h>
